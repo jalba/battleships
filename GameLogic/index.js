94ac2dd20ce7f1c7bd434cf5021e3c3c798c44b2
florianherrengt <florian.herrengt@gmail.com>
2015-11-13 15:51:01 +0000

    trim/convert white space

--- GameLogic/index.js
+++ GameLogic/index.js
@@ -1,8 +1,8 @@
 var chalk = require('chalk');
 
 var battleships = {
-	battleship: { locations: [], size: 5 },
-	destroyers: { locations: [], size: 4 } 
+    battleship: { locations: [], size: 5 },
+    destroyers: { locations: [], size: 4 }
 };
 
 var grid = {
@@ -15,7 +15,7 @@ var grid = {
     G: [],
     H: [],
     I: [],
-    J: [] 
+    J: []
 };
 
 function generateLocation(battleship) {
@@ -27,10 +27,10 @@ function generateLocation(battleship) {
     var location = [];
     if(direction === 1) { //vertical
         row = Math.floor(Math.random() * (rows.length - battleships[battleship].size + 1));
-		col = Math.floor(Math.random() * rows.length);
+        col = Math.floor(Math.random() * rows.length);
         } else { //horizontal
         row = Math.floor(Math.random() * rows.length);
-		col = Math.floor(Math.random() * (rows.length - battleships[battleship].size + 1));
+        col = Math.floor(Math.random() * (rows.length - battleships[battleship].size + 1));
     }
     for (i; i < battleships[battleship].size; i++) {
         if (direction === 1) {
@@ -44,13 +44,13 @@ function generateLocation(battleship) {
 }
 
 function cleanData() {
-	if(battleships.battleship.locations[0]) {
+    if(battleships.battleship.locations[0]) {
         for(var row in grid) {
-		    grid[row] = [];
-	    }
-	    battleships.battleship.locations = [];
-	    battleships.destroyers.locations = [];
-	}
+            grid[row] = [];
+        }
+        battleships.battleship.locations = [];
+        battleships.destroyers.locations = [];
+    }
 }
 
 function init(next) {
@@ -63,7 +63,7 @@ function init(next) {
     for(i; i < 2; i++){
         destroyerLocation = generateLocation('destroyers');
         while(doesCollide(destroyerLocation)) {
-        	destroyerLocation = generateLocation('destroyers');
+            destroyerLocation = generateLocation('destroyers');
         }
         destroyers.locations.push(destroyerLocation);
     }
@@ -72,68 +72,68 @@ function init(next) {
 
 function doesCollide(location) {
     for(var battleship in battleships) {
-    	if(battleships[battleship].locations.some(function(ship) {
-    	    var collision = location.filter(function(coordinate) {
+        if(battleships[battleship].locations.some(function(ship) {
+            var collision = location.filter(function(coordinate) {
                 return ship.indexOf(coordinate) !== -1;
             });
             return !!collision.length;
-    	})) return true;
+        })) return true;
     }
     return false;
 }
 
 function addcoordinate(coordinate) {
     grid[coordinate[0]].push(coordinate[1]);
-} 
+}
 
 function fire(coordinate, finish, next) {
     if(!coordinateExist(coordinate)) {
-    	addcoordinate(coordinate);
-    	if(isHit(coordinate)) {
-    		var ship = getBattleShipFromcoordinate(coordinate);
-    		handleHit(ship, coordinate);
-    		if(isSunk(ship)) {
-    			if(!shipsLeft()) {
+        addcoordinate(coordinate);
+        if(isHit(coordinate)) {
+            var ship = getBattleShipFromcoordinate(coordinate);
+            handleHit(ship, coordinate);
+            if(isSunk(ship)) {
+                if(!shipsLeft()) {
                     console.log(chalk.green('Congratulations, You have sunk all the battleships! You Won!!!'));
                     finish();
-    			} else {
-    				console.log(chalk.green('Great, you just sunk a ship!!!!'));
-    				next();
-    			}
-    		} else {
-    			console.log(chalk.green('Hit!!'));
-    			next();
-    		}
-    	} else {
-    		console.log(chalk.green('Miss...'));
-    	    next();
-    	}
+                } else {
+                    console.log(chalk.green('Great, you just sunk a ship!!!!'));
+                    next();
+                }
+            } else {
+                console.log(chalk.green('Hit!!'));
+                next();
+            }
+        } else {
+            console.log(chalk.green('Miss...'));
+            next();
+        }
     } else {
-    	console.log(chalk.red('You already fired on that coordinate, pick another one:'));
-    	next();
+        console.log(chalk.red('You already fired on that coordinate, pick another one:'));
+        next();
     }
 }
 
 function handleHit(battleship, coordinate) {
-	var index = battleship.indexOf(coordinate);
-	battleship[index] = 1;
+    var index = battleship.indexOf(coordinate);
+    battleship[index] = 1;
 }
 
 function getBattleShipFromcoordinate(coordinate) {
-	var battleship;
-	for(var ship in battleships) {
+    var battleship;
+    for(var ship in battleships) {
         battleship = battleships[ship].locations.filter(function(location) {
-			return location.indexOf(coordinate) !== -1;
-		})[0];
-		if(battleship && battleship.length > 0) {
-			return battleships[ship].locations[battleships[ship].locations.indexOf(battleship)];
-		}
-	}
+            return location.indexOf(coordinate) !== -1;
+        })[0];
+        if(battleship && battleship.length > 0) {
+            return battleships[ship].locations[battleships[ship].locations.indexOf(battleship)];
+        }
+    }
 }
 
 function shipsLeft() {
-    var ships = battleships.battleship.locations[0].concat(battleships.destroyers.locations[0], 
-    	                                                battleships.destroyers.locations[1]);
+    var ships = battleships.battleship.locations[0].concat(battleships.destroyers.locations[0],
+                                                        battleships.destroyers.locations[1]);
     return !isSunk(ships);
 }
 
@@ -147,13 +147,13 @@ function isHit(coordinate) {
 
 function isSunk(battleship) {
     var result = battleship.reduce(function(prev, current) {
-    	return prev + current;
+        return prev + current;
     });
     return !isNaN(result);
 }
 
 module.exports = {
-	fire: fire,
-	init: init
+    fire: fire,
+    init: init
 }
 
--- test/GameLogic.test.js
+++ test/GameLogic.test.js
@@ -7,53 +7,53 @@ var coordinate1 = 'A4';
 var coordinate2 = 'A5';
 
 describe('The GameLogic module', function() {
-	beforeEach(function() {
+    beforeEach(function() {
         sinon.spy(console, 'log');
-	});
-	afterEach(function() {
-	    console.log.restore();
-	});
-	it('should expose two methos', function() {
-		methodsArray = Object.keys(GameLogic);
-		expect(methodsArray.length).to.equal(2);
-		expect(methodsArray.indexOf('init')).not.to.equal(-1);
-		expect(methodsArray.indexOf('fire')).not.to.equal(-1);
-	});
-	it('should inform the user that game is been initialised', function() {
-		var callback = function() {};
+    });
+    afterEach(function() {
+        console.log.restore();
+    });
+    it('should expose two methos', function() {
+        methodsArray = Object.keys(GameLogic);
+        expect(methodsArray.length).to.equal(2);
+        expect(methodsArray.indexOf('init')).not.to.equal(-1);
+        expect(methodsArray.indexOf('fire')).not.to.equal(-1);
+    });
+    it('should inform the user that game is been initialised', function() {
+        var callback = function() {};
         GameLogic.init(callback);
         var calledArgs = console.log.getCall(0).args[0];
         console.log(calledArgs);
         var condition = calledArgs.indexOf('generating game grid...') > -1;
         expect(condition).to.equal(true);
-	});
-	it('should call a callback function after game is initialised', function() {
+    });
+    it('should call a callback function after game is initialised', function() {
         var callback = sinon.spy();
         GameLogic.init(callback);
         expect(callback.called).to.equal(true);
 
-	});
-	it('should inform the user the result of the play', function() {
-		var finish = function() {};
-		var callback = function() {};
+    });
+    it('should inform the user the result of the play', function() {
+        var finish = function() {};
+        var callback = function() {};
         GameLogic.fire(coordinate2, finish, callback);
         var calledArgs = console.log.getCall(0).args[0];
         var condition = (calledArgs.indexOf('Miss...') > -1 || alledArgs.indexOf('Hit!!') > -1);
         expect(condition).to.equal(true);
-	});
-	it('should inform the user the coordinate is already been used', function() {
-		var finish = function() {};
-		var callback = function() {};
-		var warning = 'You already fired on that coordinate, pick another one:';
+    });
+    it('should inform the user the coordinate is already been used', function() {
+        var finish = function() {};
+        var callback = function() {};
+        var warning = 'You already fired on that coordinate, pick another one:';
         GameLogic.fire(coordinate2, finish, callback);
         var calledArgs = console.log.getCall(0).args[0];
         var condition = calledArgs.indexOf(warning) > -1;
         expect(condition).to.equal(true);
-	});
-	it('should call a callback function after player has entered some coordinate', function() {
-		var callback = sinon.spy();
-		var finish = function() {};
+    });
+    it('should call a callback function after player has entered some coordinate', function() {
+        var callback = sinon.spy();
+        var finish = function() {};
         GameLogic.fire(coordinate1, finish, callback);
         expect(callback.called).to.equal(true);
-	});
+    });
 });
